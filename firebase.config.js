c8a4d9604dc21f0245b1b13de322d12f074cab27
Thomas Basham <Bashamtg@gmail.com>
2024-07-02 16:36:36 -0700

    added auth init code

--- firebase.config.js
+++ firebase.config.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,6 +22,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-const db = getFirestore(app)
+const db = getFirestore(app);
 
-export { db }
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export { db, auth };
